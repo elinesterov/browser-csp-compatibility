e928cd2d9bf4d96edf5ffb5aea3256b04d17bc66
Ilya Nesterov <ilya.nesterov@gmail.com>
2016-04-11 21:24:53 -0700

    Added modified child-src tests for workers

--- tests/child-src/test_child_src_worker.py
+++ tests/child-src/test_child_src_worker.py
@@ -4,3 +4,31 @@ from utils.page_model import BasePage
 
 from tests.common import generate_test_url
 
+
+@pytest.mark.parametrize("header, meta", [(True, False), (False, True)])
+def test_child_src_worker_allowed(browser, header, meta):
+    """
+    Test iframe load allowed if CSP is "child-src 'self'"
+    """
+    policy = "child-src 'self'"
+    params = "worker=true"
+    url = generate_test_url(policy, header=header, meta=meta, allow=True,
+                            fixture_url='child-src', params=params)
+    page = BasePage(browser).open(url)
+    res = page.get_test_results()
+    assert (res == 'Pass')
+
+
+@pytest.mark.parametrize("header, meta", [(True, False), (False, True)])
+def test_child_src_worker_blocked(browser, header, meta):
+    """
+    Test iframe load blocked if CSP is "child-src 'none'"
+    """
+
+    policy = "child-src 'none'"
+    params = "worker=true"
+    url = generate_test_url(policy, header=header, meta=meta, allow=False,
+                            fixture_url='child-src', params=params)
+    page = BasePage(browser).open(url)
+    res = page.get_test_results()
+    assert (res == 'Pass')
